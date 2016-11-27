bc276379ab0bf3b01c3ee37cd8bb23004bc2a1d2
aranga1 <aranga@purdue.du>
2016-11-27 13:07:26 -0500

    NEW CLOUD FOUNDRY APP

--- app.js
+++ app.js
@@ -1,28 +1,103 @@
-/*eslint-env node*/
+/*
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+  There are some minor modifications to the default Express setup
+  Each is commented and marked with [SH] to make them easy to find
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
+ */
+
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
+var path = require('path');
+var favicon = require('serve-favicon');
 var cfenv = require('cfenv');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// [SH] Require Passport
+var passport = require('passport');
 
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+// [SH] Bring in the data model
+require('./app_api/models/db');
+// [SH] Bring in the Passport config after model is defined
+require('./app_api/config/passport');
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+// [SH] Bring in the routes for the API (delete the default routes)
+var routesApi = require('./app_api/routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// [SH] Set the app_client folder to serve static resources
+app.use(express.static(path.join(__dirname, 'app_client')));
+
+// [SH] Initialise Passport before using the route middleware
+app.use(passport.initialize());
+
+// [SH] Use the API routes when path starts with /api
+app.use('/api', routesApi);
+
+app.set('port', (process.env.PORT || 8080));
+// [SH] Otherwise render the index.html page for the Angular SPA
+// [SH] This means we don't have to map all of the SPA routes in Express
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// [SH] Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+module.exports = app;
