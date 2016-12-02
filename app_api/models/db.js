c3658232d0c00aed990a8e0be965540896803368
Eehita <Eehita@pal-nat186-41-44.itap.purdue.edu>
2016-12-02 15:40:00 -0500

    changed DB url

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-var dbURI = 'mongodb://localhost/meanAuth';
-//var dbURI = 'mongodb://3fd738be-956d-4f46-ba35-e7354067d99b:b86a6be0-db94-4416-a7aa-dc93a3864e97@75.126.37.69:10001/db';
+//var dbURI = 'mongodb://localhost/meanAuth';
+var dbURI = 'mongodb://3fd738be-956d-4f46-ba35-e7354067d99b:b86a6be0-db94-4416-a7aa-dc93a3864e97@75.126.37.69:10001/db';
 
 mongoose.connect(dbURI);
 
